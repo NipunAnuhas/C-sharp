60c7b63817048f7ba55fc000b6130874c8668bfc
NipunAnuhas <140608142+NipunAnuhas@users.noreply.github.com>
2024-03-20 16:17:03 +0530

    Add files via upload

---
+++ Lab sheet 02,Q2/Lab sheet 02,Q2/Bank Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_02_Q2
+{
+    internal class Bank_Account
+    {
+        public int AccountNumber;
+        public double Balance;
+
+        public void deposit(double amount)
+        {
+            Balance = Balance + amount;
+        }
+    }
+}
---
+++ Lab sheet 02,Q2/Lab sheet 02,Q2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_02_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank_Account account = new Bank_Account();
+
+            Console.Write("Enter the account number: ");
+            account.AccountNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the deposit amount: ");
+            double amount = double.Parse(Console.ReadLine());
+
+            account.deposit(amount);
+
+            Console.WriteLine("Account number: " + account.AccountNumber);
+            Console.WriteLine("Updated Balance: " + account.Balance);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
