129b5efa4b38680cf727b2f8ddc1637b6fcb70c3
NipunAnuhas <140608142+NipunAnuhas@users.noreply.github.com>
2024-03-20 16:16:21 +0530

    Add files via upload

---
+++ Lab sheet 02,Q1/Lab sheet 02,Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_02_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            books book = new books();
+            book.Author = "Anita Desai";
+            book.Title = "Village by the sea";
+
+            Console.WriteLine("Title: " + book.Title);
+            Console.WriteLine("Author: " + book.Author);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab sheet 02,Q1/Lab sheet 02,Q1/books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_02_Q1
+{
+    internal class books
+    {
+        public String Title;
+        public String Author;
+    }
+}
