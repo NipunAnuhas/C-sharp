d2babf64390ef13c5f008e27430d1544b7876046
NipunAnuhas <140608142+NipunAnuhas@users.noreply.github.com>
2024-03-23 18:25:39 +0530

    Add files via upload

---
+++ Lab Sheet 02,Q3/Lab Sheet 02,Q3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Sheet_02_Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureTracker tracker = new TemperatureTracker();
+
+            tracker.dailyTemperature();
+            Console.WriteLine("\n");
+            tracker.Report();
+
+            Console.ReadLine();
+        }
+    }
+}
